f64bb1cde8886af5d87381a215a2eeaaadde3a34
Sushant Kumar <sushantkumar23@gmail.com>
2021-07-02 11:32:58 +0530

    Merging similar property types into groups

--- main.py
+++ main.py
@@ -3,21 +3,10 @@ from fastapi import FastAPI, HTTPException
 
 from prompts import generate_description
 from models.property_types import (
-    ApartmentListingData,
-    BuilderFloorListingData,
+    ResidentialListingData,
     LandListingData,
-    OfficeSpaceListingData,
-    PlotListingData,
-    VillaListingData,
-    PenthouseListingData,
-    IndependentHouseListingData,
-    IndustrialPlotListingData,
-    OfficeSpaceSezListingData,
-    ShopListingData,
-    ShowroomListingData,
-    WarehouseListingData
+    CommercialListingData
 )
-
 
 app = FastAPI()
 
@@ -27,105 +16,25 @@ async def root():
     return "Hello World"
 
 
-@app.post('/apartment_descriptions')
-async def generate_apartment_description(apartment_listing_data: ApartmentListingData, format: bool = False):
+@app.post('/residential_descriptions')
+async def generate_apartment_description(residential_listing_data: ResidentialListingData, format: bool = False):
     """
-    Generates descriptions for properties of type Apartment
+    Generates descriptions for residential property types
     """
-    return generate_description(apartment_listing_data, format=format)
-
-
-@app.post('/builder_floor_descriptions')
-async def generate_builder_floor_description(builder_floor_listing_data: BuilderFloorListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Builder Floor
-    """
-    return generate_description(builder_floor_listing_data, format=format)
+    return generate_description(residential_listing_data, format=format)
 
 
 @app.post('/land_descriptions')
-async def generate_land_description(land_listing_data: LandListingData, format: bool = False):
+async def land_description(land_listing_data: LandListingData, format: bool = False):
     """
-    Generates descriptions for properties of type Land
+    Generates descriptions for land property types
     """
     return generate_description(land_listing_data, format=format)
 
 
-@app.post('/office_space_descriptions')
-async def generate_office_space_description(office_space_listing_data: OfficeSpaceListingData, format: bool = False):
+@app.post('/commercial_descriptions')
+async def generate_land_description(commercial_listing_data: CommercialListingData, format: bool = False):
     """
-    Generates descriptions for properties of type Office Space
+    Generates descriptions for commercial property types
     """
-    return generate_description(office_space_listing_data, format=format)
-
-
-@app.post('/plot_descriptions')
-async def generate_plot_description(plot_listing_data: PlotListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Plot
-    """
-    return generate_description(plot_listing_data, format=format)
-
-
-@app.post('/villa_descriptions')
-async def generate_villa_description(villa_listing_data: VillaListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Villa
-    """
-    return generate_description(villa_listing_data, format=format)
-
-
-@app.post('/penthouse_descriptions')
-async def generate_penthouse_description(penthouse_listing_data: PenthouseListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Penthouse
-    """
-    return generate_description(penthouse_listing_data, format=format)
-
-
-@app.post('/independent_house_descriptions')
-async def generate_independent_house_description(independent_house_listing_data: IndependentHouseListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Independent House
-    """
-    return generate_description(independent_house_listing_data, format=format)
-
-
-@app.post('/industrial_plot_descriptions')
-async def generate_industrial_plot_description(industrial_plot_listing_data: IndustrialPlotListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Industrial Plot
-    """
-    return generate_description(industrial_plot_listing_data, format=format)
-
-
-@app.post('/office_space_sez_descriptions')
-async def generate_office_space_sez_description(office_space_sez_listing_data: OfficeSpaceSezListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Office Space SEZ
-    """
-    return generate_description(office_space_sez_listing_data, format=format)
-
-
-@app.post('/shop_descriptions')
-async def generate_shop_description(shop_listing_data: ShopListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Shop
-    """
-    return generate_description(shop_listing_data, format=format)
-
-
-@app.post('/showroom_descriptions')
-async def generate_showroom_description(showroom_listing_data: ShowroomListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Showroom
-    """
-    return generate_description(showroom_listing_data, format=format)    
-
-
-@app.post('/warehouse_descriptions')
-async def generate_warehouse_description(warehouse_listing_data: WarehouseListingData, format: bool = False):
-    """
-    Generates descriptions for properties of type Warehouse
-    """
-    return generate_description(warehouse_listing_data, format=format)    
+    return generate_description(commercial_listing_data, format=format)
--- models/property_types.py
+++ models/property_types.py
@@ -26,6 +26,28 @@ class ListingType(str, Enum):
     rent = 'rent'
 
 
+class ResidentialPropertyType(str, Enum):
+    apartment = 'apartment'
+    builder_floor = 'builder_floor'
+    penthouse = 'penthouse'
+    villa = 'villa'
+    independent_house = 'independent_house'
+
+
+class CommercialPropertyType(str, Enum):
+    office_space = 'office_space'
+    office_space_sez = 'office_space_sez'
+    shop = 'shop'
+    showroom = 'showroom'
+    warehouse = 'warehouse'
+
+
+class LandPropertyType(str, Enum):
+    land = 'land'
+    industrial_plot = 'industrial_plot'
+    plot = 'plot'
+
+
 class BaseListingData(BaseModel):
     property_type: PropertyType
     listing_type: ListingType
@@ -39,116 +61,31 @@ class BaseListingData(BaseModel):
     amenities: str
 
 
-class ApartmentListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'apartment'
+class ResidentialListingData(BaseListingData):
+    property_type: ResidentialPropertyType
     furnishing: str
+    project: Optional[str]
+    tower: Optional[str]
     bedrooms: int
     bathrooms: int
-    parking: Optional[int]
-    property_age: str
-    floor_number: int
-    total_floor_count: int
-
-
-class PenthouseListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'penthouse'
-    furnishing: str
-    bedrooms: int
-    bathrooms: int
-    parking: Optional[int]
-    property_age: str
-    floor_number: int
-    total_floor_count: int
-
-
-class VillaListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'villa'
-    area_type: Optional[str]
-    furnishing: str
-    bedrooms: int
-    bathrooms: int
-    parking: Optional[int]
-    property_age: str
-    total_floor_count: int
-
-
-class IndependentHouseListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'independent_house'
-    area_type: Optional[str]
-    furnishing: str
-    bedrooms: int
-    bathrooms: int
-    parking: Optional[int]
-    property_age: str
-    total_floor_count: int
-
-
-class BuilderFloorListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'builder_floor'
-    project: str
-    tower: str
-    furnishing: str
-    bedrooms: int
-    bathrooms: int
-    parking: Optional[int]
-    property_age: str
-    floor_number: int
-    total_floor_count: int
+    parking: int
+    property_age: Optional[str]
+    floor_number: Optional[int]
+    total_floor_count: Optional[int]
 
 
 class LandListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'land'
-    land_number: int
-
-
-class OfficeSpaceListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'office_space'
-    office_space_type: str
-    pantry: str
-    furnishing: str
-    washroom_present: str
-    parking: int
-    floor_number: int
-    total_floor_count: int
-
-
-class PlotListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'plot'
+    property_type: LandPropertyType
+    land_number: Optional[int]
     plot_number: int
 
 
-class IndustrialPlotListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'industrial_plot'
-    plot_number: int
-
-
-class OfficeSpaceSezListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'office_space_sez'
+class CommercialListingData(BaseListingData):
+    property_type: CommercialPropertyType
     office_space_type: str
     pantry: str
-    furnishing: str
-    washroom_present: str
-    parking: Optional[int]
-    floor_number: int
-    total_floor_count: int
-
-class ShopListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'shop'
-    furnishing: str
-    washroom_present: str
-    parking: Optional[int]
-    floor_number: int
-
-
-class ShowroomListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'showroom'
-    furnishing: str
-    washroom_present: str
-    parking: Optional[int]
-    floor_number: int
-
-
-class WarehouseListingData(BaseListingData):
-    property_type: Optional[PropertyType] = 'warehouse'
+    furnishing: Optional[str]
     washroom_present: str
     parking: int
+    floor_number: Optional[int]
+    total_floor_count: Optional[int]
