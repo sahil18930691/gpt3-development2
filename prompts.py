29de2688a913b8692b5d6af9c5a66eaf52b7f19c
unknown <kshitij.das@squareyards.co.in>
2021-08-06 14:45:13 +0530

    yyy

--- prompts.py
+++ prompts.py
@@ -329,9 +329,8 @@ def format_listing_data(listing_data):
         prompt_string += f"Plot Number: {listing_data['plot_number']}\n"
     
     if "floor_number" in listing_data:
-        if listing_data["floor_number"] == 0:
-            listing_data["floor_number"] == 1
-            prompt_string += f"Floor number: {listing_data['floor_number']}\n"
+        if listing_data["floor_number"] == 0
+            prompt_string += f"Floor number: {listing_data['floor_number'] == 1}\n"
         prompt_string += f"Floor number: {listing_data['floor_number']}\n"
 
     if "total_floor_count" in listing_data:
